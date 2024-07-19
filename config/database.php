b153fada209f2c74719b432d2317209ef65bcd23
kingsley achumie <kingsonly13c@gmail.com>
2024-07-19 20:11:08 +0100

    updated the boilerplate with testing config

--- config/database.php
+++ config/database.php
@@ -42,6 +42,11 @@ return [
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
+        'sqlite_testing' => [
+            'driver'   => 'sqlite',
+            'url'      => env('DATABASE_URL'),
+            'database' => ":memory:",
+        ],
 
         'mysql' => [
             'driver' => 'mysql',
@@ -125,7 +130,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
         'default' => [
